c992f5e82c503100e49c7049d6ba6ec5094d8f62
antonaorlov <aorlov927@gmail.com>
2023-05-22 22:14:03 -0400

    cnages

--- database/models/Task.js
+++ database/models/Task.js
@@ -5,13 +5,10 @@ const Task = db.define("task", {
   description: {
     type: Sequelize.STRING,
     allowNull: true, // Allow this field to be null
-    //defaultValue: "Default description", // Set a default value
   },
-
   priority: {
     type: Sequelize.STRING
   },
-
   isComplete: {
     type: Sequelize.BOOLEAN,
     allowNull: false,
